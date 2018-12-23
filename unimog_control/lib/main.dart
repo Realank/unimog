df3c71070e26150a301c07f37553c9deec54f534
yanbo.liu <yanbo.liu@ucarinc.com>
2018-12-23 22:01:36 +0800

    Initial commit

---
+++ unimog_control/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:http/http.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  Future<Response> fetchPost() {
+    return get('https://www.baidu.com');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    fetchPost();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ControlPanel(),
+    );
+  }
+}
+
+class ControlPanel extends StatefulWidget {
+  @override
+  _ControlPanelState createState() => _ControlPanelState();
+}
+
+class _ControlPanelState extends State<ControlPanel> {
+  RawDatagramSocket udpSocket;
+  int speed = 0;
+  int direction = 0;
+  @override
+  void initState() {
+    RawDatagramSocket.bind(InternetAddress.anyIPv4, 0)
+        .then((RawDatagramSocket udpSocket) {
+      this.udpSocket = udpSocket;
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  void sendData() {
+    if (this.udpSocket != null) {
+      List<int> dataToSend = [0xfa, 0x01, speed, direction, 0xfb];
+      print('Sending from ${udpSocket.address.address}:${udpSocket.port}');
+      final num =
+          udpSocket.send(dataToSend, new InternetAddress('192.168.4.1'), 8888);
+      print('Did send data on the stream.. $num');
+    }
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    udpSocket.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Text('$speed'),
+          FlatButton(
+              onPressed: () {
+                this.speed = (this.speed + 10) % 0xff;
+                sendData();
+                setState(() {});
+              },
+              child: Text('speed up'))
+        ],
+      ),
+    );
+  }
+}
